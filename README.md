51fd82751a9b6fa2c210bad43847501ec8d3ab47
nbkhope <ryumemaru@yahoo.com>
2024-05-09 21:11:04 -0400

    Integrate postgres

--- README.md
+++ README.md
@@ -5,3 +5,34 @@ Video: <https://www.youtube.com/watch?v=kNk_ycw5uM8>
 ## Adding Express.js backend
 
 Video: <https://www.youtube.com/watch?v=N9Xg9zu5v4I>
+
+## How to Run
+
+Copy `.env.example` to a new file `.env`. Then fill in the values for each variable.
+
+```
+# macOS / Linux
+cp .env.example .env
+
+# Windows
+copy .env.example .env
+```
+
+Install dependencies:
+
+```
+npm install
+```
+
+Have two terminal windows, one for backend server:
+
+```
+# todo: make npm run server
+node ./server/index.js
+```
+
+And another for the frontend client:
+
+```sh
+npm start
+```
--- server/index.js
+++ server/index.js
@@ -1,32 +1,54 @@
 const express = require('express');
-const posts = require('./posts.json')
 const cors = require('cors');
 const fs = require('fs');
+const pg = require('pg');
+require('dotenv').config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json())
 
-// HTTP  ... RESTful ......
-// GET /posts
-app.get('/posts', function(req, res) {
-  res.set('Content-Type', 'application/json')
-  res.send(posts)
-})
+async function setupApp() {
+  const pgClient = new pg.Client({
+    database: process.env.DATABASE_DATABASE,
+    password: process.env.DATABASE_PASSWORD,
+    user: process.env.DATABASE_USER,
+  });
+  await pgClient.connect();
 
-// POST /posts
-app.post('/posts', function(req, res) {
-  const newPost = {
-    title: req.body.title
-  };
+  // HTTP  ... RESTful ......
+  // GET /posts
+  app.get('/posts', function(req, res) {
+    res.set('Content-Type', 'application/json')
+    pgClient.query('SELECT * FROM posts;').then((result) => {
+      res.send(result.rows);
+    })
+  })
 
-  posts.push(newPost)
-  fs.writeFileSync('posts.json', JSON.stringify(posts, null, 2), { encoding: 'utf-8' })
+  // POST /posts
+  app.post('/posts', async function(req, res) {
+    const newPost = {
+      title: req.body.title
+    };
 
-  res.send(newPost)
-})
+    // We would typically filter out undesired properties and validate them before doing anything with database.
+    const current_user = { id: 1 }; // assume it was defined somewhere else in the authn pipeline. For the sake of this lesson, hardcode here.
+    let result
+    try {
+      result = await pgClient.query('INSERT INTO posts (title, body, user_id) VALUES ($1, $2, $3) RETURNING *', [newPost.title, newPost.body, current_user.id])
+    } catch (error) {
+      // report error, handle error, etc
+      console.error(error);
+      return res.status(500).send();
+    }
 
-app.listen(3001, function() {
-  console.log('Server is running at http://localhost:3001')
-});
+    return res.send(result.rows[0]);
+  })
+
+  app.listen(3001, function() {
+    console.log('Server is running at http://localhost:3001')
+  });
+}
+
+setupApp();
